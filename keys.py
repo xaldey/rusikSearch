1acfaddba59e90aef24e7367c15a57ecf710960c
xaldey <xaldey@mail.ru>
2020-05-18 19:37:59 +0300

    Рабочий вариант: пока только сохранение файла массива.

---
+++ keys.py
@@ -0,0 +1,6 @@
+ROOT_URL_YA = 'http://camgirlvideos.org/page/'
+HEADERS = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
+                         '(KHTML, like Gecko) Chrome/80.0.3987.132 '
+                         'Safari/537.36'}
+
+
---
+++ simple.py
@@ -0,0 +1,51 @@
+import csv
+import requests
+import time
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+
+UserAgent().chrome
+MAIN_URL = 'http://camgirlvideos.org/page/'
+FILE = 'export.csv'
+main_array = []
+
+
+def save_file(items, path):
+    with open(path, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(['Title', 'Link', 'Image Link', 'Date published'])
+        for elem in items:
+            writer.writerow([elem['title'], elem['link'], elem['image_link'], elem['date_published']])
+
+
+print('*' * 50)
+
+
+def make_array():
+    for page in range(2, 8):
+        page_link = MAIN_URL + str(page)
+        print(page_link)
+        response = requests.get(page_link, headers={'User-Agent': UserAgent().chrome})
+        # for key, value in response.request.headers.items():
+        #     print(key + ": " + value)
+        page_array = []
+        html = response.content
+        soup = BeautifulSoup(html, 'html.parser')
+        # title = soup.html.head.title.text
+        obj = soup.findAll('div', class_="hentry-pad blocks")
+        for elem in obj:
+            page_array.append({
+                'title': elem.find('h4', class_="entry-title").get_text(),
+                'link': elem.find('a').get('href'),
+                'image_link': elem.find('img').get('src'),
+                'date_published': elem.find('div', class_="metabar-pad").get_text(),
+            })
+            # print(page_array)
+        main_array.extend(page_array)
+        print('*' * 50)
+        time.sleep(1)
+        save_file(main_array, FILE)
+
+
+if __name__ == '__main__':
+    make_array()
